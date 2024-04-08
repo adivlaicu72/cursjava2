1a0fa768825c4392a506e45a806ebbcd2f7551b4
Adrian Romulus Vlaicu <adivlaicu72@gmail.com>
2024-04-08 20:37:49 +0300

    curs 9 intermediar

---
+++ src/curs9/Tester.java
@@ -0,0 +1,22 @@
+package curs9;
+
+public class Tester extends Angajat{
+	
+	private String testWritingSkills;
+	private String bugWritingSkills;
+	
+	public String getTestWritingSkills() {
+		return testWritingSkills;
+	}
+	public void setTestWritingSkills(String testWritingSkills) {
+		
+		this.testWritingSkills = testWritingSkills;
+	}
+	public String getBugWritingSkills() {
+		return bugWritingSkills;
+	}
+	public void setBugWritingSkills(String bugWritingSkills) {
+		this.bugWritingSkills = bugWritingSkills;
+	}
+
+}
