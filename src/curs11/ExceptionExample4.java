3f7291384d719002965f6c781e80b154269a131e
Adrian Romulus Vlaicu <adivlaicu72@gmail.com>
2024-04-15 20:30:04 +0300

    curs11 intermediar

---
+++ src/curs11/ExceptionExample4.java
@@ -0,0 +1,22 @@
+package curs11;
+
+import java.util.Scanner;
+
+public class ExceptionExample4 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter number 1: ");
+		
+		//Integer.parseInt()
+		int num1 = Integer.parseInt(scan.next());
+		System.out.println("Please enter number 2: ");
+		int num2 = scan.nextInt();
+		
+		
+		
+
+	}
+
+}
